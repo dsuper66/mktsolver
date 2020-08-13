76a3dc3801f1f75dcf64871060bfc69b5efb39ad
David Bullen <david.bullen@gmail.com>
2020-08-14 10:57:55 +1200

    added post

--- app/controllers/APIs/TodoController.scala
+++ app/controllers/APIs/TodoController.scala
@@ -1,19 +1,39 @@
-
 package controllers.api
- 
+
 import javax.inject._
 import play.api.mvc._
 import play.api.libs.json._
 import models.{Todo}
- 
-class TodoController @Inject()(
+
+class TodoController @Inject() (
     cc: ControllerComponents
 ) extends AbstractController(cc) {
- 
-    implicit val todoFormat = Json.format[Todo]
- 
-    def getAll = Action {
-        val todo = new Todo(1, "item 1", false)
-        Ok(Json.toJson(todo))
+
+  implicit val todoFormat = Json.format[Todo]
+
+  def getAll =
+    Action {
+      val todo = new Todo(1, "item 1", false)
+      Ok(Json.toJson(todo))
     }
+
+  def solve =
+    Action { request: Request[AnyContent] =>
+      val body: AnyContent = request.body
+      val jsonBody: Option[JsValue] = body.asJson
+
+      // Expecting json body
+      jsonBody
+        .map { json =>
+          val subCategories =
+            (json \ "sub-categories").as[List[Map[String, String]]]
+
+          val names = subCategories.map(_("elementId"))
+          Ok("Got: " + names)
+        }
+        .getOrElse {
+          BadRequest("Expecting application/json request body")
+        }
+    }
+
 }
