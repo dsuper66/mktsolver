58d47584c9bea6c8cb46e411fbb822ecdca1a685
David Bullen <david.bullen@gmail.com>
2020-08-27 22:22:28 +1200

    added implicit mapping

--- app/controllers/APIs/TodoController.scala
+++ app/controllers/APIs/TodoController.scala
@@ -61,19 +61,69 @@ class TodoController @Inject() (
         }
     }
 
+// //https://stackoverflow.com/questions/25741162/scala-reads-how-to-handle-optionmapstring-any
+//   object ContractDetails {
+//     implicit val readsMap =
+//       Reads[Map[String, Any]](m => Reads.mapReads[Any](anyReads).reads(m))
+//     implicit val reads = Json.reads[ContractDetails]
+//     val anyReads = Reads[Any](m => metaValueToJsValue(m))
 
-      case class ModelElement(
-        elementId: String,
-        elementTypeId: String,
-        properties: Map[String,String])
-        // properties: String)        
+//     def metaValueToJsValue(m: JsValue): JsResult[Any] = {
+//       m match {
+//         case JsBoolean(b) => JsSuccess(b)
+//         case JsNumber(n)  => JsSuccess(n)
+//         case JsString(s)  => JsSuccess(s)
+//         case JsArray(arr) => {
+//           val list = arr.map(metaValueToJsValue)
+//           JsSuccess(list)
+//         }
+//         case JsNull => JsSuccess(null)
+//         //case x => JsFailure(x.toString())
+//         case JsObject(m) => {
+//           val m1 = m.map(f => (f._1, metaValueToJsValue(f._2))).toMap
+//           JsSuccess(m1)
+//         }
+//       }
+//     }
+//   }
 
-      case class ModelElements(modelElements: Seq[ModelElement])
-      
-      object ModelElement {
-        implicit val reads = Json.reads[ModelElement]
-      }  
-    
+  case class ModelElement(
+      elementId: String,
+      elementTypeId: String,
+      properties: Map[String, Any]
+  )
+  // properties: String)
+
+  case class ModelElements(modelElements: Seq[ModelElement])
+
+  object ModelElement {
+
+
+    implicit val readsMap =
+      Reads[Map[String, Any]](m => Reads.mapReads[Any](anyReads).reads(m))
+    val anyReads = Reads[Any](m => metaValueToJsValue(m))
+
+    def metaValueToJsValue(m: JsValue): JsResult[Any] = {
+      m match {
+        case JsBoolean(b) => JsSuccess(b)
+        case JsNumber(n)  => JsSuccess(n)
+        case JsString(s)  => JsSuccess(s)
+        case JsArray(arr) => {
+          val list = arr.map(metaValueToJsValue)
+          JsSuccess(list)
+        }
+        case JsNull => JsSuccess(null)
+        //case x => JsFailure(x.toString())
+        case JsObject(m) => {
+          val m1 = m.map(f => (f._1, metaValueToJsValue(f._2))).toMap
+          JsSuccess(m1)
+        }
+      }
+    }
+
+    implicit val reads = Json.reads[ModelElement]
+  }
+
   def solve2 =
     Action { request: Request[AnyContent] =>
       val body: AnyContent = request.body
@@ -81,20 +131,17 @@ class TodoController @Inject() (
 
       jsonBody
         .map { json =>
- 
-          val outString = (json \"elements" ).as[Seq[ModelElement]]
+          val outString = (json \ "elements").as[Seq[ModelElement]]
 
           // Json.parse(s).as[Seq[ModelElement]]
 
           // // val elements: Map[String, Element] =
           // //   (json \ "elements").as[Map[String,Element]]
           // // // val jsonAst = json.toString // or JsonParser(source)
-      
 
           // // var outString = ""
           // // for ((element, arrayOfProperties) <- elements)
           // //   outString += (s"element: $element\n")
-
 
           Ok("Got data:\n" + outString)
         }
