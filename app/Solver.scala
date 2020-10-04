11b971537c6dec6e06f49129fa4ab76b68456ca9
David Bullen <david.bullen@gmail.com>
2020-10-04 15:59:52 +1300

    add reduced cost

--- app/Solver.scala
+++ app/Solver.scala
@@ -8,32 +8,32 @@ object MathModel {
   case class Element(elementId: String, bus: String)
 
   case class ModelElement(
-      elementId: String,
-      elementType: String,
-      properties: Map[String, Any]
-  )
+                           elementId: String,
+                           elementType: String,
+                           properties: Map[String, Any]
+                         )
 
   case class ModelElements(modelElements: Seq[ModelElement])
 
   case class ConstraintDef(
-      constraintType: String,
-      elementType: String,
-      varType: String,
-      inEquality: String,
-      rhsProperty: String,
-      rhsValue: Double,
-      multProperty: String
-  )
+                            constraintType: String,
+                            elementType: String,
+                            varType: String,
+                            inEquality: String,
+                            rhsProperty: String,
+                            rhsValue: Double,
+                            multProperty: String
+                          )
 
   case class ConstraintComp(
-      constraintType: String,
-      elementType: String,
-      propertyMap: String,
-      varType: String,
-      multParentProperty: String,
-      multValue: Double,
-      multProperty: String
-  )
+                             constraintType: String,
+                             elementType: String,
+                             propertyMap: String,
+                             varType: String,
+                             multParentProperty: String,
+                             multValue: Double,
+                             multProperty: String
+                           )
 
   // case class Property(name: String, value: Any)
 
@@ -47,8 +47,8 @@ object MathModel {
     def metaValueToJsValue(m: JsValue): JsResult[Any] = {
       m match {
         case JsBoolean(b) => JsSuccess(b)
-        case JsNumber(n)  => JsSuccess(n)
-        case JsString(s)  => JsSuccess(s)
+        case JsNumber(n) => JsSuccess(n)
+        case JsString(s) => JsSuccess(s)
         case JsArray(arr) => {
           val list = arr.map(metaValueToJsValue)
           JsSuccess(list)
@@ -65,10 +65,12 @@ object MathModel {
     //Json deserialiser for ModelElement (and needs to be after the above implicit for Reads)
     implicit val reads = Json.reads[ModelElement]
   }
+
   object ConstraintDef {
     //need Json deserializer for type
     implicit val reads = Json.reads[ConstraintDef]
   }
+
   object ConstraintComp {
     //need Json deserializer for type
     implicit val reads = Json.reads[ConstraintComp]
@@ -80,18 +82,20 @@ object MathModel {
                         constraintId: String,
                         value: Double
                       )
+
   case class Constraint(
-                       constraintId: String,
-                       constraintType: String,
-                       elementId: String,
-                       inequality: String,
-                       rhsValue: Double
+                         constraintId: String,
+                         constraintType: String,
+                         elementId: String,
+                         inequality: String,
+                         rhsValue: Double
                        )
+
   case class Variable(
                        varId: String,
                        varType: String,
-                         elementId: String
-                       )
+                       elementId: String
+                     )
 
   //Data
   var constraintIds: Seq[String] = Seq()
@@ -101,66 +105,88 @@ object MathModel {
   var varFactors: Seq[VarFactor] = Seq()
   var constraints: Seq[Constraint] = Seq()
   var variables: Seq[Variable] = Seq()
+  var reducedCosts: Seq[Double] = Seq()
 
   //Populate
-//  def addConstraintIfNew(key: String): Unit = {
-//    if (!constraintIds.contains(key)) constraintIds = constraintIds :+ key
-//  }
-//  def addVarIfNew(key: String): Unit = {
-//    if (!varIds.contains(key)) varIds = varIds :+ key
-//  }
-  def addConstraint(constraintType: String,elementId: String,inEquality: String,rhsValue: Double): String = {
+  //  def addConstraintIfNew(key: String): Unit = {
+  //    if (!constraintIds.contains(key)) constraintIds = constraintIds :+ key
+  //  }
+  //  def addVarIfNew(key: String): Unit = {
+  //    if (!varIds.contains(key)) varIds = varIds :+ key
+  //  }
+  def addConstraint(constraintType: String, elementId: String, inEquality: String, rhsValue: Double): String = {
     val constraintId = s"${constraintType}.${elementId}"
     if (!constraints.exists(c => c.constraintId == constraintId)) {
-      constraints = constraints :+ Constraint(constraintId,constraintType,elementId,inEquality,rhsValue)
+      constraints = constraints :+ Constraint(constraintId, constraintType, elementId, inEquality, rhsValue)
     }
     constraintId
   }
-  def addVar(elementId: String,varType: String): String = {
+
+  def addVar(elementId: String, varType: String): String = {
     val varId = s"${elementId}.${varType}"
     if (!variables.exists(mathVar => mathVar.varId == varId)) {
-      variables = variables :+ Variable(varId,varType,elementId)
+      variables = variables :+ Variable(varId, varType, elementId)
     }
     varId
   }
 
   def setVarFactor(
-      varId: String,
-      constraintId: String,
-      value: Double
-  ): Unit = {
-    val varFactor = VarFactor(varId,constraintId,value)
+                    varId: String,
+                    constraintId: String,
+                    value: Double
+                  ): Unit = {
+    val varFactor = VarFactor(varId, constraintId, value)
     if (!varFactors.contains(varFactor)) varFactors = varFactors :+ varFactor
-//    varFactors = varFactors.filter(v => !(v.varId == varId && v.constraintId == constraintId))
-//    varFactors = varFactors :+ VarFactor(varId,constraintId,value)
+    //    varFactors = varFactors.filter(v => !(v.varId == varId && v.constraintId == constraintId))
+    //    varFactors = varFactors :+ VarFactor(varId,constraintId,value)
   }
 
-//Report
+  //Report
   def constraintsString: String = {
     constraintIds.mkString("\n")
   }
+
   def varsString: String = {
     varIds.mkString("\n")
   }
+
   def varFactorsString: String = {
     varFactors.map(_.toString).mkString("\n")
   }
 
+  def varFactorsForConstraint(c: Constraint): Seq[Double] = {
+    //If there is a varFactor for this constraint+var then add it otherwise add zero
+    variables.map(v =>
+      varFactors.find(vF => (vF.varId, vF.constraintId) == (v.varId, c.constraintId))
+      match {
+        case Some(optVF) => optVF.value
+        case None => 0.0
+      }
+    )
+  }
+
   //Solve
   def solveModel: String = {
-    //A row of varFactors for each constraint
-    for (c <- constraints.filter(_.constraintType != "objective")) {
+    //Reduced costs for the objective
+    //and a row of varFactors for each constraint
+    //    for (c <- constraints.filter(_.constraintType != "objective")) {
+    for (c <- constraints) {
+      if (c.constraintType == "objective") { //expecting only one
+        reducedCosts = varFactorsForConstraint(c)
+      }
       //If there is a varFactor for this constraint+var then add it otherwise add zero
-      val varFactorRow = variables.map(v =>
-        varFactors.find(vF => (vF.varId, vF.constraintId) == (v.varId, c.constraintId))
-        match {
-          case Some(optVF) => optVF.value
-          case None => 0.0
-        }
-      )
-      varFactorRows = varFactorRows :+ varFactorRow
+      //      val varFactorRow = variables.map(v =>
+      //        varFactors.find(vF => (vF.varId, vF.constraintId) == (v.varId, c.constraintId))
+      //        match {
+      //          case Some(optVF) => optVF.value
+      //          case None => 0.0
+      //        }
+      //      )
+      else {
+        varFactorRows :+= varFactorsForConstraint(c)
+      }
     }
-    varFactorRows.map(_.toString).mkString("\n")
+    s"${varFactorRows.map(_.toString).mkString("\n")} \nobjective\n${reducedCosts.toString()}"
   }
 
 }
